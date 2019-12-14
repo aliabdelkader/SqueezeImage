4b374ebed46463912d0abb3d6c7d8b0110ad2349
Ali Abdelkader <aly.abdelkader@aucegypt.edu>
2019-12-14 21:27:55 +0200

    reduce batch size, increase image width

--- train.sh
+++ train.sh
@@ -6,13 +6,13 @@ python main.py \
 --model_name "${model_name}" \
 --dataset_root_path "/mnt/sdb1/ali/cityscapes" \
 --dataset_config_path "cityscapes/cityscapes.yaml" \
---batch_size 64 \
+--batch_size 32 \
 --imageset_path "cityscapes/imagesets" \
 --results_dir "results/${model_name}" \
 --learning_rate "0.001" \
 --number_epochs "100" \
---image_height "256" \
---image_width "512" \
+--image_height "512" \
+--image_width "1024" \
 --output_classes "20" \
 --device "cuda" \
 --num_gpus "2"
