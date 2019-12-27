6d992530b5f7c183edd7e4df4ec6fc2e9879fa0a
Ali Abdelkader <aly.abdelkader@aucegypt.edu>
2019-12-27 23:23:43 +0200

    fix paths

--- cityscapes/prepare_cityscapes.sh
+++ cityscapes/prepare_cityscapes.sh
@@ -1,7 +1,7 @@
 #python create_imagesets.py --dataset_root "/mnt/sdb1/ali/cityscapes" --output_dir "/home/mrafaat/ali/SqueezeImage/cityscapes/imagesets"
 python cityscapes/calculate_stats.py \
 --dataset_root_path "/raid/ali/cityscapes" \
---imageset_path "cityscapes/imagesets" \
+--imageset_path "imagesets" \
 --dataset_config_path "cityscapes/cityscapes.yaml" \
 --image_height "512" \
 --image_width "1024" \
