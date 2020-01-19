1f2795b564cbb850bd81110f00f6ecacab77a680
Ali Abdelkader <aly.abdelkader@aucegypt.edu>
2020-01-19 22:08:44 +0200

    evaluate to calculate inference time

--- evaluate.sh
+++ evaluate.sh
@@ -12,14 +12,14 @@
 #--model_name "${model_name}" \
 #--model_path "results/SqueezeImage/best_99.pth" \
 #--output_classes "20" \
-export CUDA_VISIBLE_DEVICES="5"
+export CUDA_VISIBLE_DEVICES=
 export model_name="SqueezeImage"
-sudo $(which python) CityscapesEvaluator.py \
---dataset_root_path "/raid/ali/cityscapes" \
+$(which python) CityscapesEvaluator.py \
+--dataset_root_path "/home/fusionresearch/Datasets/cityscapes" \
 --imageset_path "cityscapes/imagesets" \
 --dataset_config_path "cityscapes/cityscapes.yaml" \
---results_dir "/raid/ali/results/${model_name}" \
---device "cuda" \
+--results_dir "results/${model_name}" \
+--device "cpu" \
 --image_height "512" \
 --image_width "1024" \
 --model_name "${model_name}" \
