44803b8c7d7970b59de7ea738e7c51644443ba0c
Ali Abdelkader <aly.abdelkader@aucegypt.edu>
2020-01-19 23:27:00 +0200

    infer on cpu

--- infer_semantic.sh
+++ infer_semantic.sh
@@ -4,7 +4,7 @@ python SemanticIKitti_infer.py \
 --dataset_root_path "/home/fusionresearch/SemanticKitti/dataset" \
 --imageset_path "semantickitti/imageset" \
 --results_dir $(pwd)'/SemanticKitti' \
---device 'cuda' \
+--device 'cpu' \
 --model_name "SqueezeImage" \
 --model_path "results/SqueezeImage/best_179.pth" \
 --output_classes '20'
